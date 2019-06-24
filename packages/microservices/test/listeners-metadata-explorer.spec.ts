e94ff65ee1f54d8c3efaafeee7de2e3456b1b138
Andrey <geass1000@gmail.com>
2019-06-24 16:03:08 +0300

    test(microservices): add tests for `class-method-pattern` logic

--- packages/microservices/test/listeners-metadata-explorer.spec.ts
+++ packages/microservices/test/listeners-metadata-explorer.spec.ts
@@ -5,6 +5,9 @@ import { Client } from '../decorators/client.decorator';
 import { MessagePattern } from '../decorators/message-pattern.decorator';
 import { Transport } from '../enums/transport.enum';
 import { ListenerMetadataExplorer } from '../listener-metadata-explorer';
+import {
+  PATTERN_METADATA,
+} from '../constants';
 
 describe('ListenerMetadataExplorer', () => {
   const pattern = { pattern: 'test' };
@@ -41,47 +44,246 @@ describe('ListenerMetadataExplorer', () => {
     instance = new ListenerMetadataExplorer(scanner);
   });
   describe('explore', () => {
-    let scanFromPrototype: sinon.SinonSpy;
-    beforeEach(() => {
-      scanFromPrototype = sinon.spy(scanner, 'scanFromPrototype');
-    });
-    it(`should call "scanFromPrototype" with expected arguments`, () => {
-      const obj = new Test();
-      instance.explore(obj);
+    let test: Test;
+    const sandbox = sinon.createSandbox();
+    const methodKey = 'anyKey';
+    const classPrototype = Test.prototype;
 
-      const { args } = scanFromPrototype.getCall(0);
-      expect(args[0]).to.be.eql(obj);
-      expect(args[1]).to.be.eql(Object.getPrototypeOf(obj));
+    let lmeExploreMethodMetadataStub: sinon.SinonStub;
+    let lmeExploreClassMetadataStub: sinon.SinonStub;
+
+    beforeEach(() => {
+      test = new Test();
+      sandbox
+        .stub((instance as any), 'metadataScanner')
+        .value({ scanFromPrototype(arg1, arg2, arg3) { return arg3(methodKey); } });
+      lmeExploreMethodMetadataStub = sinon
+        .stub(instance, 'exploreMethodMetadata');
+      lmeExploreClassMetadataStub = sinon
+        .stub(instance, 'exploreClassMetadata');
     });
+
+    afterEach(() => {
+      lmeExploreMethodMetadataStub.restore();
+      lmeExploreClassMetadataStub.restore();
+      sandbox.restore();
+    });
+
+    it(`should call 'scanFromPrototype with expected arguments`, () => {
+      sandbox
+        .stub((instance as any), 'metadataScanner')
+        .value({ scanFromPrototype(arg1, arg2, arg3) {} });
+      const lmeScanFromPrototypeSpy = sinon
+        .spy((instance as any).metadataScanner, 'scanFromPrototype');
+
+      instance.explore(test);
+      expect(lmeScanFromPrototypeSpy.args[0][0]).to.be.equal(test);
+      expect(lmeScanFromPrototypeSpy.args[0][1]).to.be.equal(classPrototype);
+
+      lmeScanFromPrototypeSpy.restore();
+    });
+
+    it(`should call 'exploreMethodMetadata with expected arguments`, () => {
+      instance.explore(test);
+      expect(lmeExploreMethodMetadataStub.args[0][0]).to.be.equal(test);
+      expect(lmeExploreMethodMetadataStub.args[0][1]).to.be.equal(classPrototype);
+      expect(lmeExploreMethodMetadataStub.args[0][2]).to.be.equal(methodKey);
+    });
+
+    describe(`when 'exploreMethodMetadata' returns 'null'`, () => {
+      it(`should return 'null'`, () => {
+        lmeExploreMethodMetadataStub
+          .returns(null);
+
+        const result = instance.explore(test);
+        expect(result).to.be.null;
+      });
+    });
+
+    it(`should call 'exploreClassMetadata with expected arguments`, () => {
+      const exploreMethodMetadata: any = { use: `getData` };
+      lmeExploreMethodMetadataStub
+        .returns(exploreMethodMetadata);
+
+      try {
+        instance.explore(test);
+      } catch (error) {}
+      expect(lmeExploreClassMetadataStub.args[0][0]).to.be.equal(classPrototype);
+    });
+
+    describe(`when 'exploreClassMetadata' returns 'null' pattern (case 1,2,3)`, () => {
+      it(`should return correct metadata`, () => {
+        const exploreMethodMetadata: any = { use: `getData` };
+        lmeExploreMethodMetadataStub
+          .returns(exploreMethodMetadata);
+        lmeExploreClassMetadataStub
+          .returns({ pattern: null });
+
+        const result = instance.explore(test);
+        expect(result).to.be.equal(exploreMethodMetadata);
+      });
+    });
+
+    describe(`when happens 4th case`, () => {
+      it(`should return correct metadata`, () => {
+        const classPattern: any = { controller: `user` };
+        const methodPattern: any = `getData`;
+        const resultPattern = `user/getData`;
+        lmeExploreMethodMetadataStub
+          .returns({ pattern: methodPattern });
+        lmeExploreClassMetadataStub
+          .returns({ pattern: classPattern });
+
+        const result = instance.explore(test);
+        expect(result).to.be.deep.equal({ pattern: resultPattern });
+      });
+    });
+
+    describe(`when happens 5th case`, () => {
+      it(`should return correct metadata`, () => {
+        const classPattern: any = { controller: { use: `user`, id: 5 } };
+        const methodPattern: any = `getData`;
+        const resultPattern = `getData`;
+        lmeExploreMethodMetadataStub
+          .returns({ pattern: methodPattern });
+        lmeExploreClassMetadataStub
+          .returns({ pattern: classPattern });
+
+        const result = instance.explore(test);
+        expect(result).to.be.deep.equal({ pattern: resultPattern });
+      });
+    });
+
+    describe(`when happens 6th case`, () => {
+      it(`should return correct metadata`, () => {
+        const classPattern: any = { controller: { use: `user` } };
+        const methodPattern: any = { use: `getData` };
+        const resultPattern = { controller: { use: `user` }, use: `getData` };
+        lmeExploreMethodMetadataStub
+          .returns({ pattern: methodPattern });
+        lmeExploreClassMetadataStub
+          .returns({ pattern: classPattern });
+
+        const result = instance.explore(test);
+        expect(result).to.be.deep.equal({ pattern: resultPattern });
+      });
+    });
+
+    describe(`when happens 7th case`, () => {
+      it(`should return correct metadata`, () => {
+        const classPattern: any = { controller: `user` };
+        const methodPattern: any = { use: `getData` };
+        const resultPattern = { controller: `user`, use: `getData` };
+        lmeExploreMethodMetadataStub
+          .returns({ pattern: methodPattern });
+        lmeExploreClassMetadataStub
+          .returns({ pattern: classPattern });
+
+        const result = instance.explore(test);
+        expect(result).to.be.deep.equal({ pattern: resultPattern });
+      });
+    });
+
+    describe(`when happens 8th case`, () => {
+      it(`should return correct metadata`, () => {
+        const classPattern: any = { controller: { use: `user` } };
+        const methodPattern: any = null;
+        const resultPattern = { controller: { use: `user` } };
+        lmeExploreMethodMetadataStub
+          .returns({ pattern: methodPattern });
+        lmeExploreClassMetadataStub
+          .returns({ pattern: classPattern });
+
+        const result = instance.explore(test);
+        expect(result).to.be.deep.equal({ pattern: resultPattern });
+      });
+    });
+
+    describe(`when happens 9th case`, () => {
+      it(`should return correct metadata`, () => {
+        const classPattern: any = { controller: `user` };
+        const methodPattern: any = null;
+        const resultPattern = { controller: `user` };
+        lmeExploreMethodMetadataStub
+          .returns({ pattern: methodPattern });
+        lmeExploreClassMetadataStub
+          .returns({ pattern: classPattern });
+
+        const result = instance.explore(test);
+        expect(result).to.be.deep.equal({ pattern: resultPattern });
+      });
+    });
+
   });
+
   describe('exploreMethodMetadata', () => {
     let test: Test;
     beforeEach(() => {
       test = new Test();
     });
-    it(`should return undefined when "handlerType" metadata is undefined`, () => {
-      const metadata = instance.exploreMethodMetadata(
-        test,
-        Object.getPrototypeOf(test),
-        'noPattern',
-      );
-      expect(metadata).to.eq(undefined);
+
+    describe(`when "handlerType" metadata is undefined`, () => {
+      it(`should return null`, () => {
+        const metadata = instance.exploreMethodMetadata(
+          test,
+          Object.getPrototypeOf(test),
+          'noPattern',
+        );
+        expect(metadata).to.be.null;
+      });
     });
-    it(`should return pattern properties when "handlerType" metadata is not undefined`, () => {
-      const metadata = instance.exploreMethodMetadata(
-        test,
-        Object.getPrototypeOf(test),
-        'test',
-      );
-      expect(metadata).to.have.keys([
-        'isEventHandler',
-        'methodKey',
-        'targetCallback',
-        'pattern',
-      ]);
-      expect(metadata.pattern).to.eql(pattern);
+
+    describe(`when "handlerType" metadata is not undefined`, () => {
+      it(`should return pattern properties`, () => {
+        const metadata = instance.exploreMethodMetadata(
+          test,
+          Object.getPrototypeOf(test),
+          'test',
+        );
+        expect(metadata).to.have.keys([
+          'isEventHandler',
+          'methodKey',
+          'targetCallback',
+          'pattern',
+        ]);
+        expect(metadata.pattern).to.eql(pattern);
+      });
     });
   });
+
+  describe('exploreClassMetadata', () => {
+    const test: Test = new Test();
+    const classPrototype = Object.getPrototypeOf(test);
+
+    describe(`when class metadata is undefined or null`, () => {
+      it(`should return object where pattern field is a 'null'`, () => {
+        const classPattern = null;
+
+        Reflect.defineMetadata(PATTERN_METADATA, undefined, classPrototype.constructor);
+
+        const metadata = instance.exploreClassMetadata(classPrototype);
+        expect(metadata).to.be.deep.equal({ pattern: classPattern });
+      });
+    });
+
+    describe(`when class metadata is defined`, () => {
+      it(`should return object where pattern field is a defined data`, () => {
+        const classPatterns = [
+          `cat`, 4,
+          { path: 'user' },
+          Symbol('User'),
+        ];
+
+        classPatterns.forEach((classPattern) => {
+          Reflect.defineMetadata(PATTERN_METADATA, classPattern, classPrototype.constructor);
+
+          const metadata = instance.exploreClassMetadata(classPrototype);
+          expect(metadata).to.be.deep.equal({ pattern: { controller: classPattern } });
+        });
+      });
+    });
+  });
+
   describe('scanForClientHooks', () => {
     it(`should returns properties with @Client decorator`, () => {
       const obj = new Test();
