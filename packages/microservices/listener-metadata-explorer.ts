f3ecef16539f8533b332e2499aaf2a08508853aa
Andrey <geass1000@gmail.com>
2019-06-24 16:02:17 +0300

    feature(microservices): add handling of different class and method patterns

--- packages/microservices/listener-metadata-explorer.ts
+++ packages/microservices/listener-metadata-explorer.ts
@@ -1,5 +1,5 @@
 import { Controller } from '@nestjs/common/interfaces/controllers/controller.interface';
-import { isFunction, isUndefined, isNil } from '@nestjs/common/utils/shared.utils';
+import { isFunction, isUndefined, isNil, isString } from '@nestjs/common/utils/shared.utils';
 import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 import {
   CLIENT_CONFIGURATION_METADATA,
@@ -13,23 +13,118 @@ import * as MsInterfaces from './interfaces';
 export class ListenerMetadataExplorer {
   constructor(private readonly metadataScanner: MetadataScanner) {}
 
+  /**
+   * Extracts patterns from class and method decorators, combines them
+   * and returns result.
+   *
+   * 1. Default Logic
+   * Class: -
+   * Method: { use: `getData` }
+   * Result: { use: `getData` }
+   * Route: `{use:getData}`
+   *
+   * 2. Default Logic
+   * Class: -
+   * Method: -
+   * Result: -
+   * Route: -
+   *
+   * 3. Default Logic
+   * Class: -
+   * Method: `getData`
+   * Result: `getData`
+   * Route: `getData`
+   *
+   * 4.
+   * Class: `user`
+   * Method: `getData`
+   * Result: `user/getData`
+   * Route: `user/getData`
+   *
+   * 5. Class decorators aren't supported
+   * Class: { use: `user`, id: 5 }
+   * Method: `getData`
+   * Result: `getData`
+   * Route: `getData`
+   *
+   * 6.
+   * Class: { use: `user` }
+   * Method: { use: `getData` }
+   * Result: { controller: { use: `user` }, use: `getData` }
+   * Route: `{controller:{use:user}/use:getData}`
+   *
+   * 7.
+   * Class: `user`
+   * Method: { use: `getData` }
+   * Result: { controller: `user`, use: `getData` }
+   * Route: `{controller:user/use:getData}`
+   *
+   * 8.
+   * Class: { use: `user` }
+   * Method: -
+   * Result: { controller: { use: `user` } }
+   * Route: `{controller:{use:user}}`
+   *
+   * 9.
+   * Class: `user`
+   * Method: -
+   * Result: { controller: `user` }
+   * Route: `{controller:user}`
+   *
+   * @param  {Controller} instance - instance of class (controller)
+   * @returns MsInterfaces.MethodDescription[]
+   */
   public explore(instance: Controller): MsInterfaces.MethodDescription[] {
     const instancePrototype = Object.getPrototypeOf(instance);
     return this.metadataScanner.scanFromPrototype<
         Controller,
         MsInterfaces.MethodDescription
       >(instance, instancePrototype, method => {
+        // Gets a method metadata
         const methodMetadata = this.exploreMethodMetadata(instance, instancePrototype, method);
 
+        // If method doesn't have a decorator, callback will return null
         if (isNil(methodMetadata)) {
           return null;
         }
 
+        // Gets a class metadata
         const classMetadata = this.exploreClassMetadata(instancePrototype);
+
+        // Cases 1, 2, 3
+        // Returns the default method pattern
+        if (isNil(classMetadata.pattern)) {
+          return methodMetadata;
+        }
+
+        let pattern: any;
+        // isObject - Ex: 6, 7; isNil - Ex: 8, 9
+        // If method pattern is a string/number (no object/undefined/null) value
+        if (!(typeof methodMetadata.pattern === `object`) && !isNil(methodMetadata.pattern)) {
+          // Cases 4, 5
+          // If class pattern is a string value
+          pattern = isString(classMetadata.pattern.controller)
+            // Case 4
+            // Sets pattern as `controller/method` string pattern
+            ? `${classMetadata.pattern.controller}/${methodMetadata.pattern}`
+            // Case 5
+            // Sets pattern as `method` string pattern
+            : methodMetadata.pattern;
+        } else {
+          // Cases 6, 7, 8, 9
+          // If method pattern is not null
+          pattern = !isNil(methodMetadata.pattern)
+            // Case 6, 7
+            // Sets pattern as `class + method` object pattern
+            ? { ...classMetadata.pattern, ...methodMetadata.pattern }
+            // Case 8, 9
+            // Sets pattern as `class` object pattern
+            : { ...classMetadata.pattern };
+        }
 
         return {
           ...methodMetadata,
-          pattern: { ...classMetadata.pattern, ...methodMetadata.pattern },
+          pattern,
         };
       });
   }
